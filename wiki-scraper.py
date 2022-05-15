6d456e3afa87288fd45917c26f9fe6c12d8ba0a2
Praxicus <nerfgunz@hotmail.com>
2022-05-14 19:29:37 -0700

    test 1

--- wiki-scraper.py
+++ wiki-scraper.py
@@ -1,27 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+from flask import Flask, request, json
 
-def scrape(link):
-	response = requests.get(url = link,)
+app = Flask(__name__)
+
+@app.get('/scrape')
+def scrape():
+	page = request.args.get('page')
+	page = page.replace(' ', '_')
+	page.lower()
+	response = requests.get(url = ('https://en.wikipedia.org/wiki/' + page))
 
 	soup = BeautifulSoup(response.content, 'html.parser')
 	list(soup.children)
 
 	info = soup.find(id='mw-content-text').find_all('p')
+	
+	article = ''
 
-	f = open('wiki.txt', 'w')
 	for para in info:
-		f.write(para.get_text())
-		f.write('\n')
+		article = article + para.get_text()
 
+	wiki = {
+		'page': page.lower(),
+		'link': 'https://en.wikipedia.org/wiki/' + page,
+		'article': article
+	}
 
-page = sys.argv[1]
-page = page.replace(' ', '_')
-page.lower()
-
-link = ('https://en.wikipedia.org/wiki/' + page)
-
-print(link + '\n')
-
-scrape(link)
+	# f = open('wiki.txt', 'w', encoding="utf-8")
+	# for para in info:
+	# 	f.write(para.get_text())
+	# 	f.write('\n')
+		
+	return json.dumps(wiki)
